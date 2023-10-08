899b3569ef43709848e7380f053b46eb4478bad9
GABG <git@gabg.email>
2023-10-08 20:07:19 +0200

    Add IETF language code support in mkv audio tracks

--- scripts/mkv-audio-track-lang-rename.ps1
+++ scripts/mkv-audio-track-lang-rename.ps1
@@ -7,8 +7,18 @@ param (
 #Returns $null if the code is not mapped
 function Convert-LanguageCode {
 	param (
-		[Parameter(Mandatory=$true)][string]$LanguageCode
+		[Parameter(Mandatory=$true)][string]$LanguageCode,
+		[Parameter(Mandatory=$true)][AllowEmptyString()][string]$LanguageCodeIetf
 	)
+
+	if($LanguageCodeIetf -ne ''){
+		$LanguageName = [System.Globalization.CultureInfo]::GetCultureInfoByIetfLanguageTag($LanguageCodeIetf).EnglishName
+		if($LanguageName -match "\(\w\w\)"){
+			return $null
+		} else {
+			return $LanguageCode
+		}
+	}
 
 	switch ($LanguageCode) {
 		eng {"English"}
@@ -25,18 +35,19 @@ function Invoke-TrackRenaming {
 	)
 
 	# Get all audio tracks
-	$Tracks = mkvmerge --identification-format json --identify $WorkingFile | ConvertFrom-Json | Where-Object codec -eq "SubRip/SRT"
+	$Tracks = (mkvmerge --identification-format json --identify $WorkingFile | ConvertFrom-Json).tracks | Where-Object type -eq "audio"
 
 	#Process each track
 	foreach($Track in $Tracks){
+		$Track.language
 		#Get track language
-		$TrackLanguage = Convert-LanguageCode -LanguageCode $Track.language
+		$TrackLanguage = Convert-LanguageCode -LanguageCode $Track.properties.language -LanguageCodeIetf $Track.properties.language_ietf
 
 		#Actually modify the file only if the language code was converted and the run is not a test
 		if (($null -ne $TrackLanguage) -and ($TestRun -eq $false)) {
 			Write-Error "Edit logic"
 		} elseif ($TestRun -eq $true) {
-			Write-Host "File: $WorkingFile `n>> Track: $($Track.id) `n>> Language: $($Track.language) converted to $TrackLanguage"
+			Write-Host "File: $WorkingFile `n>> Track: $($Track.id) `n>> Language: $($Track.properties.language) converted to $TrackLanguage"
 		} else {
 			Write-Error "Not supported"
 		}
@@ -44,12 +55,12 @@ function Invoke-TrackRenaming {
 }
 
 # Check if provided path is a directory or a file
-if( (Get-Item -Path $WorkingPath) -Is [System.IO.DirectoryInfo] ){
-	$FileArray = Get-ChildItem -Path $($WorkingPath.Trim()+'/*') -Include *.flac
+if((Get-Item -Path $WorkingPath) -Is [System.IO.DirectoryInfo]){
+	$FileArray = Get-ChildItem -Path $($WorkingPath.Trim()+'/*') -Include *.mkv
 
 	foreach($File in $FileArray){
-		Assert-File -WorkingFile $File
+		Invoke-TrackRenaming -WorkingFile $File
 	}
 } else {
-	Assert-File -WorkingFile $WorkingPath
+	Invoke-TrackRenaming -WorkingFile $WorkingPath
 }
