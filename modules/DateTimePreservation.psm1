b0d86636762f7cfc6ed06a445c6154ecd7ae4cdf
GABG <git@gabg.email>
2023-10-11 19:35:39 +0200

    Add an universal DateTime preservation module

---
+++ modules/DateTimePreservation.psm1
@@ -0,0 +1,26 @@
+function Get-ItemDateTimeAttributes {
+	param (
+		[Parameter(Mandatory=$true)][string]$Path
+	)
+
+	$ItemAttributes = Get-ItemProperty -Path $Path
+
+	return [PSCustomObject]@{
+		CreationTime = $ItemAttributes.CreationTimeUtc
+		LastAccessTime = $ItemAttributes.LastAccessTimeUtc
+		LastWriteTime = $ItemAttributes.LastWriteTimeUtc
+	}
+}
+
+function Set-ItemDateTimeAttributes {
+	param (
+		[Parameter(Mandatory=$true)][string]$Path,
+		[Parameter(Mandatory=$true)][PSCustomObject]$Data
+	)
+
+	Set-ItemProperty -Path $Path -Name CreationTimeUtc -Value $Data.CreationTime
+
+	Set-ItemProperty -Path $Path -Name LastAccessTimeUtc -Value $Data.LastAccessTime
+
+	Set-ItemProperty -Path $Path -Name LastWriteTimeUtc -Value $Data.LastWriteTime
+}
